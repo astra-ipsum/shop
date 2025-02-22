e29fc0da9a06b0177e69455a3f9848aa04d57bc5
astra-ipsum <astra.ipsum@gmail.com>
2025-02-22 14:06:40 -0500

    Create redirect.js
    Adding correct reload system

---
+++ redirect.js
@@ -0,0 +1,7 @@
+(function() {
+    var redirectPath = sessionStorage.getItem("redirectPath");
+    if (redirectPath) {
+        sessionStorage.removeItem("redirectPath"); // Prevent looping
+        window.history.replaceState({}, "", redirectPath); // Restore the original URL
+    }
+})();
